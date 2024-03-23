e2afc3a34f8d5f9ebbc70b7857d4dca65a07f1d1
Michael Epps <mep053@gmail.com>
2024-03-23 19:10:39 -0400

    Fix deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'astro/config'
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  plugins: [],
-  integrations: [tailwind()]
-});
+    site: 'https://blog.michaelepps.me',
+    plugins: [],
+    integrations: [tailwind()],
+})
