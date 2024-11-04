35f56c43548fa7fc88d6c993ba7499b178ccc681
Michael Epps <mep053@gmail.com>
2024-11-03 20:13:48 -0800

    Add callouts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'astro/config'
 
+import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
     site: 'https://blog.michaelepps.me',
     plugins: [],
-    integrations: [tailwind()],
+    integrations: [tailwind(), mdx()],
     prefetch: true,
 })
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,9 +1,28 @@
 import { rssSchema } from '@astrojs/rss'
-import { z, defineCollection } from 'astro:content'
+import { z } from 'astro/zod';
+import { defineCollection } from 'astro:content'
+
+export const Series = {
+    Calli: "Calli"
+}
+
+export const Part = {
+    One: '1',
+    Two: '2',
+    Three: '3',
+}
+
+const seriesEntrySchema = z.object({
+    seriesEntry: z.object({
+        of: z.nativeEnum(Series),
+        part: z.nativeEnum(Part)
+    }).optional()
+}).optional();
+
 
 const writingsCollection = defineCollection({
     type: 'content',
-    schema: rssSchema,
+    schema: z.intersection(rssSchema, seriesEntrySchema)
 })
 
 export const collections = {
